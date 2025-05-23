f35cc03782571b59be5f5eb7ca727565844b8f4e
Nguyen Khanh Linh <xrkhanhlinh@gmail.com>
2025-05-23 07:52:08 +0000

    update

--- src/main.java
+++ src/main.java
@@ -11,6 +11,7 @@ public class main {
 
         QuanLyVe quanLyVe = new QuanLyVe();
         QuanLyKhachHang quanLyKhachHang = new QuanLyKhachHang();
+        QuanLyChuyenBay quanLyChuyenBay = new QuanLyChuyenBay();
 
         int luaChon;
 
@@ -20,7 +21,7 @@ public class main {
             System.out.println("2. Hiển thị tất cả vé");
             System.out.println("3. Sửa vé");
             System.out.println("4. Xóa vé");
-            System.out.println("5. Hiển thị thông tin chuyến bay");
+            System.out.println("5. Quản lý chuyến bay");
             System.out.println("6. Quản lý khách hàng");
             System.out.println("0. Thoát");
             System.out.print("Nhập lựa chọn của bạn: ");
@@ -41,7 +42,7 @@ public class main {
                     quanLyVe.deleteVe(sc);
                     break;
                 case 5:
-                    chuyenBay.hienThiThongTin();
+                    menuChuyenBay(sc, quanLyChuyenBay);
                     break;
                 case 6:
                     menuKhachHang(sc, quanLyKhachHang);
@@ -82,6 +83,41 @@ public class main {
                     break;
                 case 4:
                     qlkh.deleteKhachHang(sc);
+                    break;
+                case 0:
+                    System.out.println("🔙 Trở lại menu chính.");
+                    break;
+                default:
+                    System.out.println("Lựa chọn không hợp lệ.");
+            }
+        } while (chon != 0);
+    }
+
+    public static void menuChuyenBay(Scanner sc, QuanLyChuyenBay qlcb) {
+        int chon;
+        do {
+            System.out.println("\n--- QUẢN LÝ CHUYẾN BAY ---");
+            System.out.println("1. Thêm chuyến bay");
+            System.out.println("2. Hiển thị tất cả các chuyến bay");
+            System.out.println("3. Sửa chuyến bay");
+            System.out.println("4. Xóa chuyến bay");
+            System.out.println("0. Quay lại menu chính");
+            System.out.print("Chọn chức năng: ");
+            chon = sc.nextInt();
+            sc.nextLine(); 
+
+            switch (chon) {
+                case 1:
+                    qlcb.addChuyenBay(sc);
+                    break;
+                case 2:
+                    qlcb.showChuyenBay();
+                    break;
+                case 3:
+                    qlcb.editChuyenBay(sc);
+                    break;
+                case 4:
+                   qlcb.deleteChuyenBay(sc);
                     break;
                 case 0:
                     System.out.println("🔙 Trở lại menu chính.");
