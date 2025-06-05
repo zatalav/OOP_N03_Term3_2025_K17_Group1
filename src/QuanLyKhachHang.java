fe699f51251ee68a0bff70eb443c7822bcf9dd30
Nguyen Khanh Linh <xrkhanhlinh@gmail.com>
2025-06-05 00:39:48 +0000

    update

--- src/QuanLyKhachHang.java
+++ src/QuanLyKhachHang.java
@@ -1,57 +1,52 @@
-package src;
+package review;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
-public class QuanLyKhachHang {
-    private ArrayList<KhachHang> danhSachKH = new ArrayList<>();
+public class QuanLyKhachHang extends manager<KhachHang> {
 
-    public void addKhachHang(Scanner sc) {
+    @Override
+    protected KhachHang nhap() {
         KhachHang kh = new KhachHang();
         kh.nhapThongTin();
-        danhSachKH.add(kh);
-        System.out.println("Đã thêm khách hàng thành công!");
+        return kh;
     }
 
-    public void editKhachHang(Scanner sc) {
-        System.out.print("Nhập mã khách hàng cần sửa: ");
-        String maKH = sc.nextLine();
-        for (int i = 0; i < danhSachKH.size(); i++) {
-            if (danhSachKH.get(i).getMaKhachHang().equals(maKH)) {
-                System.out.println("Nhập thông tin mới cho khách hàng:");
-                KhachHang khMoi = new KhachHang();
-                khMoi.nhapThongTin();
-                danhSachKH.set(i, khMoi);
-                System.out.println("Đã sửa khách hàng thành công!");
-                return;
+    public static void main(String[] args) {
+        QuanLyKhachHang ql = new QuanLyKhachHang();
+        Scanner sc = new Scanner(System.in);
+        int chon;
+
+        do {
+            System.out.println("\n===== MENU =====");
+            System.out.println("1. Thêm khách hàng");
+            System.out.println("2. Sửa khách hàng");
+            System.out.println("3. Xóa khách hàng");
+            System.out.println("0. Thoát");
+            System.out.print("Chọn: ");
+            chon = Integer.parseInt(sc.nextLine());
+
+            switch (chon) {
+                case 1:
+                    ql.add();
+                    break;
+                case 2:
+                    System.out.print("Nhập mã khách hàng cần sửa: ");
+                    ql.edit();
+                    break;
+                case 3:
+                    System.out.print("Nhập mã khách hàng cần xóa: ");
+                    ql.delete();
+                    break;
+                case 0:
+                    System.out.println("Thoát chương trình.");
+                    break;
+                default:
+                    System.out.println("Lựa chọn không hợp lệ!");
             }
-        }
-        System.out.println("Không tìm thấy mã khách hàng này!");
-    }
 
-    public void deleteKhachHang(Scanner sc) {
-        System.out.print("Nhập mã khách hàng cần xóa: ");
-        String maKH = sc.nextLine();
-        for (int i = 0; i < danhSachKH.size(); i++) {
-            if (danhSachKH.get(i).getMaKhachHang().equals(maKH)) {
-                danhSachKH.remove(i);
-                System.out.println("Đã xóa khách hàng thành công!");
-                return;
-            }
-        }
-        System.out.println("Không tìm thấy mã khách hàng này!");
-    }
+        } while (chon != 0);
 
-    public void showKhachHang() {
-        if (danhSachKH.isEmpty()) {
-            System.out.println("Danh sách khách hàng rỗng.");
-            return;
-        }
-
-        for (int i = 0; i < danhSachKH.size(); i++) {
-            System.out.println("Khách hàng số " + (i + 1) + ":");
-            danhSachKH.get(i).hienThiThongTin();
-            System.out.println("------------------------------");
-        }
+        ql.close();
+        sc.close();
     }
 }
